92ca4406292efcbbcca07c1ed0a0db087e1b2e47
unknown <joeyk2k@gmail.com>
2024-05-19 12:00:35 +0900

    testing heat map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,7 @@
 import MarkerClusterGroup from "react-leaflet-cluster";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import L from "leaflet";
-
+import "leaflet.heat";
 import "leaflet/dist/leaflet.css";
 import "leaflet-geosearch/dist/geosearch.css";
 // import "./map.css";
@@ -19,26 +19,45 @@ const markerIcon = new L.Icon({
   iconSize: new L.Point(40, 47),
 });
 
-const LeafletgeoSearch = () => {
+// const LeafletgeoSearch = () => {
+//   const map = useMap();
+
+//   useEffect(() => {
+//     const provider = new OpenStreetMapProvider();
+//     const searchControl = new GeoSearchControl({
+//       provider,
+//       showMarker: false,
+//     });
+
+//     map.addControl(searchControl);
+
+//     // Return a cleanup function that removes the control
+//     return () => {
+//       map.removeControl(searchControl);
+//     };
+//   }, [map]);
+//   // Return null since this component doesn't render anything
+//   return null;
+// };
+
+const Heatmap = ({ stores }) => {
   const map = useMap();
 
   useEffect(() => {
-    const provider = new OpenStreetMapProvider();
-    const searchControl = new GeoSearchControl({
-      provider,
-      showMarker: false,
-    });
+    const points = stores
+      ? stores.map((address) => {
+          return [address.lat, address.lng, 1]; // lat lng intensity
+        })
+      : [];
 
-    map.addControl(searchControl);
-
-    // Return a cleanup function that removes the control
-    return () => {
-      map.removeControl(searchControl);
-    };
-  }, [map]);
-
-  // Return null since this component doesn't render anything
-  return null;
+    L.heatLayer(points, {
+      minOpacity: 0.5,
+      maxZoom: map.getMaxZoom(),
+      max: 1.0,
+      radius: 15,
+      blur: 15,
+    }).addTo(map);
+  }, []);
 };
 
 const Location = ({ stores }) => {
@@ -58,63 +77,64 @@ const Location = ({ stores }) => {
   return (
     <MapContainer
       style={{ height: "500px" }}
-      center={[20.547376955226042, 1.3115999578870117]}
-      zoom={2}
+      center={[29.4241, -98.4936]}
+      zoom={12}
       minZoom={2}
       scrollWheelZoom={true}
     >
-      <LeafletgeoSearch />
+      {" "}
+      <Heatmap stores={stores} />
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <MarkerClusterGroup chunkedLoading>
-        {stores.map((address, index) => (
-          <Marker
-            icon={customIcon}
-            key={index}
-            position={[address.lat, address.lng]}
-            title={address.store}
-          >
-            <Popup>
-              <div className="border border-gray-300 p-4 rounded-lg w-64 bg-gray-50">
-                <div>
-                  <h2 className="text-lg font-semibold mb-2">
-                    {address.store
-                      ? address.store.replace("#038;", "")
-                      : "Store Name"}
-                  </h2>
-                </div>
-                <p className="text-sm leading-6">
-                  {address.address && (
-                    <span>
-                      {address.address}
-                      <br />
-                    </span>
-                  )}
-                  {address.city && (
-                    <span>
-                      {address.city}
-                      <br />
-                    </span>
-                  )}
-                  {address.country && <span>{address.country}</span>}
-                </p>
-                {address.url && (
-                  <a
-                    href={address.url}
-                    className="mt-4 block text-blue-600 hover:underline"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Visit Store Website
-                  </a>
-                )}
+      {/* <MarkerClusterGroup chunkedLoading> */}
+      {/* {stores.map((address, index) => (
+        <Marker
+          icon={customIcon}
+          key={index}
+          position={[address.lat, address.lng]}
+          title={address.store}
+        >
+          <Popup>
+            <div className="border border-gray-300 p-4 rounded-lg w-64 bg-gray-50">
+              <div>
+                <h2 className="text-lg font-semibold mb-2">
+                  {address.store
+                    ? address.store.replace("#038;", "")
+                    : "Store Name"}
+                </h2>
               </div>
-            </Popup>
-          </Marker>
-        ))}
-      </MarkerClusterGroup>
+              <p className="text-sm leading-6">
+                {address.address && (
+                  <span>
+                    {address.address}
+                    <br />
+                  </span>
+                )}
+                {address.city && (
+                  <span>
+                    {address.city}
+                    <br />
+                  </span>
+                )}
+                {address.country && <span>{address.country}</span>}
+              </p>
+              {address.url && (
+                <a
+                  href={address.url}
+                  className="mt-4 block text-blue-600 hover:underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Visit Store Website
+                </a>
+              )}
+            </div>
+          </Popup>
+        </Marker>
+      ))} */}
+      {/* </MarkerClusterGroup> */}
     </MapContainer>
   );
 };
